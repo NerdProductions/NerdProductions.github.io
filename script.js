0c382a926472e57ce73e2bc2096579a0033e6b53
NerdProductions <45341920+NerdProductions@users.noreply.github.com>
2020-03-16 12:34:37 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Be a Brit
 var colurs = {
-    colurnames: ["Salmon", "CadetBlue", "Olive", "Chocolate", "DarkKhaki", "DodgerBlue"],
-    colurphrase1: ["Are you a bear?", "Do you work for NASA?", "Do you like Italy?", "Are you from Switzerland?", "G'day mate!", "Are you a Giants fan?"],
-    colurphrase2a: ["Please don't eat me!", "I wanna go to space when I grow up!", "I do BECAUSE PIZZA.", "Which way is it to the yodeling class?", "Do you want to play some rugby?", "Go Dodgers!"],
+    colurnames: ["Salmon", "CadetBlue", "Olive", "Chocolate", "DarkKhaki", "DodgerBlue", "IndianRed", "Maroon"],
+    colurphrase1: ["Are you a bear?", "Do you work for NASA?", "Do you like Italy?", "Are you from Switzerland?", "G'day mate!", "Are you a Giants fan?", "Do you want some dosa?", "Did you watch Superbowl 53?"],
+    colurphrase2a: ["Please don't eat me!", "I wanna go to space when I grow up!", "I do BECAUSE PIZZA.", "Which way is it to the yodeling class?", "Do you want to play some rugby?", "Go Dodgers!", "I prefer naan.", "The halftime score was Maroon: 5, Rams: 3."],
     colurphrase2b: []
 };
 var colurcycle = 0;
@@ -29,10 +29,10 @@ item.addEventListener("click", itemclicker);
 var arrows = document.getElementsByClassName("arrow")
 var arrowf = document.getElementById("arrowf");
 var arrowfclick = function() {
-    if (colurcycle < 5) { 
+    if (colurcycle < 7) { 
         colurcycle = colurcycle + 1;
     }
-    else if (colurcycle === 5) {
+    else if (colurcycle === 7) {
         colurcycle = 0;
     }
     item.style.backgroundColor = colurs.colurnames[colurcycle];
@@ -47,7 +47,7 @@ var arrowbclick = function() {
         colurcycle = colurcycle - 1;
     }
     else if (colurcycle === 0) {
-        colurcycle = 5;
+        colurcycle = 7;
     }
     item.style.backgroundColor = colurs.colurnames[colurcycle];
     itemtxt.textContent = colurs.colurnames[colurcycle];
