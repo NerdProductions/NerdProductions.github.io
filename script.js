2c5bafffe0354b3ec55f14c0a24f78dc69cce6db
NerdProductions <45341920+NerdProductions@users.noreply.github.com>
2020-03-16 12:27:40 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Be a Brit
 var colurs = {
-    colurnames: ["Salmon", "CadetBlue", "Olive", "Chocolate", "DarkKhaki"],
-    colurphrase1: ["Are you a bear?", "Do you work for NASA?", "Do you like Italy?", "Are you from Switzerland?", "G'day mate!"],
-    colurphrase2a: ["Please don't eat me!", "I wanna go to space when I grow up!", "I do BECAUSE PIZZA.", "Which way is it to the yodeling class?", "Do you want to play some rugby?"],
+    colurnames: ["Salmon", "CadetBlue", "Olive", "Chocolate", "DarkKhaki", "DodgerBlue"],
+    colurphrase1: ["Are you a bear?", "Do you work for NASA?", "Do you like Italy?", "Are you from Switzerland?", "G'day mate!", "Are you a Giants fan?"],
+    colurphrase2a: ["Please don't eat me!", "I wanna go to space when I grow up!", "I do BECAUSE PIZZA.", "Which way is it to the yodeling class?", "Do you want to play some rugby?", "Go Dodgers!"],
     colurphrase2b: []
 };
 var colurcycle = 0;
@@ -29,10 +29,10 @@ item.addEventListener("click", itemclicker);
 var arrows = document.getElementsByClassName("arrow")
 var arrowf = document.getElementById("arrowf");
 var arrowfclick = function() {
-    if (colurcycle < 4) { 
+    if (colurcycle < 5) { 
         colurcycle = colurcycle + 1;
     }
-    else if (colurcycle === 4) {
+    else if (colurcycle === 5) {
         colurcycle = 0;
     }
     item.style.backgroundColor = colurs.colurnames[colurcycle];
@@ -47,7 +47,7 @@ var arrowbclick = function() {
         colurcycle = colurcycle - 1;
     }
     else if (colurcycle === 0) {
-        colurcycle = 4;
+        colurcycle = 5;
     }
     item.style.backgroundColor = colurs.colurnames[colurcycle];
     itemtxt.textContent = colurs.colurnames[colurcycle];
