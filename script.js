ececcaf09a237088d36ff48e7982fd1386df4a03
NerdProductions <45341920+NerdProductions@users.noreply.github.com>
2020-04-02 14:50:20 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Be a Brit
 var colurs = {
-    colurnames: ["Salmon", "CadetBlue", "Olive", "Chocolate", "DodgerBlue", "BurlyWood", "Chartreuse", "Tomato", "SteelBlue", "FireBrick", "ForestGreen", "Peru", "Orange"],
-    colurphrase1: [ "Are you a bear?", "Have you ever gone to space?", "Do you like Italy?", "Are you from Switzerland?", "Are you a Giants fan?", "Do ya need anything from me?", "Care to have a drink?", "Are you a European?", "Have you ever been to Detroit?", "Do ya think you could take me?", "Have you donated to my save the forests charity?", "Don't you think it is strange that I'm not the same colour as the Peruvian Flag?", "Do you want to hear a joke?",],
+    colurnames: ["Salmon", "CadetBlue", "Olive", "Chocolate", "DodgerBlue", "BurlyWood", "Chartreuse", "Tomato", "SteelBlue", "FireBrick", "ForestGreen", "Peru", "Orange", "Coral"],
+    colurphrase1: [ "Are you a bear?", "Have you ever gone to space?", "Do you like Italy?", "Are you from Switzerland?", "Are you a Giants fan?", "Do ya need anything from me?", "Care to have a drink?", "Are you a European?", "Have you ever been to Detroit?", "Do ya think you could take me?", "Have you donated to my save the forests charity?", "Don't you think it is strange that I'm not the same colour as the Peruvian Flag?", "Do you want to hear a joke?", "G'day Mate! Have you ever been to the Great Barrier Reef?"],
     colourphraseError: ["Please select one"],
-    colurphrase2a: ["Please don't eat me!", "I wanna go there too when I grow up!", "Same! I do BECAUSE PIZZA.", "I'm from there, too.", "Dodgers are better, you Giant.", "*Grunts*", "Let me get that for you. Today's special is especially aromatic.", "Some of your kind used to think me poisonous.", "Did you see South Detroit? It's my hometown.", "Let me teach you a lesson about my birthplace: the firey pits of the underowrld.", "Thank you for your service, strange mammalian creature.", "I've started a petition targeted at the owner of the Internet.", "Then orange you glad I already have some jokes lined up!"],
-    colurphrase2b: ["Thank the fish gods.", "I wanna go to space when I grow up!", "But what about PIZZA?", "I'm 65% cacao, by the way.", "Go Dodgers!", "Then leave.", "That's okay. We have specialty preztels, too.", "I am tasty in a salad.", "You should go, it's my favorite city.", "Then begone!", "Just sign here and I'll get you started.", "To each their own, I suppose.", "Orange you glad I haven't already began a joke?",]
+    colurphrase2a: ["Please don't eat me!", "I wanna go there too when I grow up!", "Same! I do BECAUSE PIZZA.", "I'm from there, too.", "Dodgers are better, you Giant.", "*Grunts*", "Let me get that for you. Today's special is especially aromatic.", "Some of your kind used to think me poisonous.", "Did you see South Detroit? It's my hometown.", "Let me teach you a lesson about my birthplace: the firey pits of the underowrld.", "Thank you for your service, strange mammalian creature.", "I've started a petition targeted at the owner of the Internet.", "Then orange you glad I already have some jokes lined up!", "I hope you did not get coral poisoning."],
+    colurphrase2b: ["Thank the fish gods.", "I wanna go to space when I grow up!", "But what about PIZZA?", "I'm 65% cacao, by the way.", "Go Dodgers!", "Then leave.", "That's okay. We have specialty preztels, too.", "I am tasty in a salad.", "You should go, it's my favorite city.", "Then begone!", "Just sign here and I'll get you started.", "To each their own, I suppose.", "Orange you glad I haven't already began a joke?", "You should go. Here's a link: https://www.natureaustralia.org.au/what-we-do/our-priorities/oceans/ocean-stories/reef-cam-underwater/."]
 };
 var colurcycle = 0;
 //Click!
@@ -59,10 +59,10 @@ var arrows = document.getElementsByClassName("arrow")
 var arrowf = document.getElementById("arrowf");
 var arrowfclick = function() {
     console.log("step10");
-    if (colurcycle < 12) { 
+    if (colurcycle < 13) { 
         colurcycle = colurcycle + 1;
     }
-    else if (colurcycle === 12) {
+    else if (colurcycle === 13) {
         colurcycle = 0;
     }
     item.style.backgroundColor = colurs.colurnames[colurcycle];
@@ -80,7 +80,7 @@ var arrowbclick = function() {
         colurcycle = colurcycle - 1;
     }
     else if (colurcycle === 0) {
-        colurcycle = 12;
+        colurcycle = 13;
     }
     item.style.backgroundColor = colurs.colurnames[colurcycle];
     itemtxt.textContent = colurs.colurnames[colurcycle];
